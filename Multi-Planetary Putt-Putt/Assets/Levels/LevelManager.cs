97c6920e239bf6f8859c7e3bba628f20ef04cdc6
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2025-10-18 19:09:00 -0600

    Finished Level Selector

---
+++ Multi-Planetary Putt-Putt/Assets/Levels/LevelManager.cs
@@ -0,0 +1,136 @@
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public enum ELevel
+{
+    None,
+    LevelOne,
+    LevelTwo,
+    LevelThree,
+    LevelFour,
+    LevelFive
+}
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    [SerializeField] private List<Button> levelButtons = new List<Button>();
+    private List<string> levelNames = new List<string>
+    {
+        "SampleScene"
+    };
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if (!PlayerPrefs.HasKey("CompletedLevels"))
+        {
+            PlayerPrefs.SetInt("CompletedLevels", 0);
+        }
+
+        AvailableLevels();
+    }
+
+    public void PlayLastUnlockedLevel()
+    {
+        LoadLevel(PlayerPrefs.GetInt("CompletedLevels"));
+    }
+
+    public void CompletedLevel(ELevel levelCompleted)
+    {
+        switch(levelCompleted)
+        {
+            case ELevel.LevelOne:
+                UpdateLevelCompletedProgress(1);
+                break;
+            case ELevel.LevelTwo:
+                UpdateLevelCompletedProgress(2);
+                break;
+            case ELevel.LevelThree:
+                UpdateLevelCompletedProgress(3);
+                break;
+            case ELevel.LevelFour:
+                UpdateLevelCompletedProgress(4);
+                break;
+            case ELevel.LevelFive:
+                UpdateLevelCompletedProgress(5);
+                break;
+        }
+    }
+
+
+    public void OpenLevel(ELevel selectedLevel)
+    {
+        switch(selectedLevel)
+        {
+            case ELevel.LevelOne:
+                LoadLevel(0);
+                break;
+            case ELevel.LevelTwo:
+                LoadLevel(1);
+                break;
+            case ELevel.LevelThree:
+                LoadLevel(2);
+                break;
+            case ELevel.LevelFour:
+                LoadLevel(3);
+                break;
+            case ELevel.LevelFive:
+                LoadLevel(4);
+                break;
+        }
+    }
+
+    private void LoadLevel(int levelNumber)
+    {
+        if (!(levelNumber >= 0 && levelNumber < levelNames.Count)) { return; }
+        SceneManager.LoadScene(levelNames[levelNumber]);
+    }
+
+    private void UpdateLevelCompletedProgress(int levelCompleted)
+    {
+        if (levelCompleted > PlayerPrefs.GetInt("CompletedLevels"))
+        {
+            PlayerPrefs.SetInt("CompletedLevels", levelCompleted);
+        } 
+    }
+
+
+
+    private void AvailableLevels()
+    {
+        if (levelButtons.Count == 0) { return; }
+
+        int availableLevels;
+        int unAvailableLevels;
+
+        for (availableLevels = 0; availableLevels <=
+            PlayerPrefs.GetInt("CompletedLevels"); availableLevels++)
+        {
+            if (availableLevels >= 0 && availableLevels < levelButtons.Count)
+            {
+                levelButtons[availableLevels].gameObject.SetActive(true);
+                levelButtons[availableLevels].onClick.AddListener(() => LoadLevel(availableLevels));
+            }
+        }
+
+        for (unAvailableLevels = availableLevels; unAvailableLevels < levelButtons.Count; unAvailableLevels++)
+        {
+            if (unAvailableLevels >= 0 && unAvailableLevels < levelButtons.Count)
+            {
+                levelButtons[unAvailableLevels].gameObject.SetActive(false);
+            }
+        }
+    }
+}
