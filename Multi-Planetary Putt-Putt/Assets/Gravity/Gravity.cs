920221cf6a90b4d63fb68d3d4fec1e4b3e9db3f3
Chai-Rex Lin <chai.rex.lin@gmail.com>
2025-10-18 18:45:56 -0600

    refactor slightly

--- Multi-Planetary Putt-Putt/Assets/Gravity/Gravity.cs
+++ Multi-Planetary Putt-Putt/Assets/Gravity/Gravity.cs
@@ -1,39 +1,25 @@
 using UnityEngine;
 
-public class HasGravity : MonoBehaviour {
+public class Gravity : MonoBehaviour {
 
 
     [Header("References")]
     [SerializeField] private Rigidbody2D rigidbody2d;
 
-    void Start() {
-        //GravityManager.attractors.Add(rigidbody2d);
-    }
-
     private void OnDestroy() {
         GravityManager.attractors.Remove(rigidbody2d);
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (!CompareTag("Gravity")) return;
-        Debug.Log("Gravity enter");
         GravityManager.attractors.Add(rigidbody2d);
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
         if (!CompareTag("Gravity")) return;
-        Debug.Log("Gravity exit");
         GravityManager.attractors.Remove(rigidbody2d);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        collision.rigidbody.linearDamping = rigidbody2d.sharedMaterial.friction;
 
-    }
-
-    private void OnCollisionExit(Collision collision) {
-        collision.rigidbody.linearDamping = 0;
-    }
 
 }
