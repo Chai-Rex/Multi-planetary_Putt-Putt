0554acf806466ba8d730131a43df324ae61ce264
Grb The Hrmit <113816703+grobinsonbarr@users.noreply.github.com>
2025-10-18 22:33:45 -0600

    adding atmospheres

--- Multi-Planetary Putt-Putt/Assets/Gravity/Atmosphere.cs
+++ Multi-Planetary Putt-Putt/Assets/Gravity/Atmosphere.cs
@@ -27,13 +27,11 @@ public class Atmosphere : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (!CompareTag("Atmosphere")) return;
-        collision.attachedRigidbody.linearDamping = 1.0f;
-        Debug.Log("set " + collision.name + " to damping " + linearDamping);
+        collision.attachedRigidbody.linearDamping += linearDamping;
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
         if (!CompareTag("Atmosphere")) return;
-        collision.attachedRigidbody.linearDamping = 0.0f;
-        Debug.Log("set " + collision.name + " to damping " + linearDamping);
+        collision.attachedRigidbody.linearDamping = Mathf.Max(collision.attachedRigidbody.linearDamping - linearDamping, 0);
     }
 }
