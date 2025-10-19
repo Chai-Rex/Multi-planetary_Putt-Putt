bd17c681d6980610bceee06271a0dfde900b928c
Grb The Hrmit <113816703+grobinsonbarr@users.noreply.github.com>
2025-10-19 14:34:58 -0600

    starting audio setup

---
+++ Multi-Planetary Putt-Putt/Assets/Audio/Audio_Scripts/EmitOnCollision.cs
@@ -0,0 +1,30 @@
+using AudioSystem;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class EmitOnCollision : MonoBehaviour
+{
+    SoundEmitter emitter;
+    AudioSource source;
+
+    public float cd = 0.5f;
+    private float lastPlayedTime = 0;
+
+    private void Awake()
+    {
+        emitter = GetComponentInChildren<SoundEmitter>();
+        source = GetComponentInChildren<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            if (emitter && Time.timeSinceLevelLoad - lastPlayedTime >= cd)
+            {
+                lastPlayedTime = Time.timeSinceLevelLoad;
+                source.Play();
+            }
+        }
+    }
+}
